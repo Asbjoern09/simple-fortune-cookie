d9e7616ed46a5348e12ca98307d007d2c49a1cf6
Charlie Warnes <97235557+CharlieFredW@users.noreply.github.com>
2024-08-15 12:49:18 +0200

    Update build-docker.sh

--- CI-scripts/push-docker.sh
+++ CI-scripts/push-docker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "$docker_password" | docker login docker.io --username "$docker_username" --password-stdin
-docker push "docker.io/$docker_username/simple-fortune-cookie:1.0-${GIT_COMMIT::8}" 
-docker push "docker.io/$docker_username/simple-fortune-cookie:latest" &
+docker push "docker.io/$docker_username/simple-fortune-cookie-${IMAGE}:1.0-${GIT_COMMIT::8}" 
+docker push "docker.io/$docker_username/simple-fortune-cookie-${IMAGE}:latest" &
 wait
