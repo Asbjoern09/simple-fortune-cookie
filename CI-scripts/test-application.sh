0aa725dc0d6356905e6c2a9d86006e3c0d03f3e8
CharlieFredW <chwar22@student.sdu.dk>
2024-08-16 20:39:33 +0000

    Fixed deployment file and fixed test application script

--- CI-scripts/test-application.sh
+++ CI-scripts/test-application.sh
@@ -2,7 +2,8 @@
 set -e
 
 echo "Deploying application to test environment"
-kubectl apply -f k8s/test-deployment.yaml
+kubectl apply -f manifests/development/redis
+kubectl apply -f manifests/development/
 
 echo "Waiting for the application to be ready"
 sleep 30 
@@ -13,11 +14,11 @@ if curl --fail http://13.60.2.166:31448; then
 else
     echo "Application is not reachable"
 
-    kubectl delete -f k8s/test-deployment.yaml
+    kubectl delete -f kubectl apply -f manifests/development/
     exit 1
 fi
 
 echo "Cleaning up the test environment"
-kubectl delete -f k8s/test-deployment.yaml
+kubectl delete -f kubectl apply -f manifests/development/
 
 echo "Test completed"
