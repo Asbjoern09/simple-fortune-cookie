e5e1c78b184e137b833caa2d35271758ce8e547a
Asbjørn Andreasen <asbjorn454@gmail.com>
2024-08-17 09:12:34 +0000

    fix test script

--- CI-scripts/test-application.sh
+++ CI-scripts/test-application.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 echo "Checking application availability"
-if curl --fail http://13.60.2.166:31448; then
+if curl --fail http://13.60.14.234:31565; then
     echo "Application is reachable!"
 else
     echo "Application is not reachable"
